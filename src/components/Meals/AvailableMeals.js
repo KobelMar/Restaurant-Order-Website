5db9fc7c9894fd0b9015cbfdd2660f3025848d03
KobelMar <max.marcisiak@gmail.com>
2022-12-13 17:31:26 +0100

    added mealArray to Database, Loading- and Error Handling.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://foodorderapp-5c9f8-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+
+      const fetchedMeals = [];
+
+      for (let key in data) {
+        const meal = {
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        };
+        fetchedMeals.push(meal);
+      }
+
+      setMeals(fetchedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <Card individualClass={classes.MealsLoading}>
+        <p>is Loading...</p>
+      </Card>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <Card individualClass={classes.httpError}>
+        <p>Error</p>
+      </Card>
+    );
+  }
+
+  const mealsList = meals.map((meal) => {
     return <MealItem meal={meal} key={meal.id} />;
   });
 
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -4,6 +4,6 @@ import classes from "./Card.module.css"
 
 export default function Card(props) {
   return (
-    <div className={classes.cardContainer}>{props.children}</div>
+    <div className={`${classes.cardContainer} ${props.individualClass}` }>{props.children}</div>
   )
 }
